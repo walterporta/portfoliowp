c934ad1d3f7ff82de6f77025ccf2471987860977
walterporta <walterandresporta@gmail.com>
2023-07-13 07:37:26 -0300

    agregue botton a las card

--- src/projects/cards/PortfolioCard.jsx
+++ src/projects/cards/PortfolioCard.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
 const PortfolioCard = () => {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-    const handleMouseEnter = () => {
+  const handleMouseEnter = () => {
     setIsHovered(true);
   };
 
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
-  
-    return(
-        <div
+
+  return (
+    <div
       className="flex flex-col md:flex-row items-center justify-center border border-transparent rounded-md"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
@@ -36,13 +36,17 @@ const PortfolioCard = () => {
               </div>
               <br />
               <p className="font-bold">Tecnologías utilizadas:</p>
+              <div className="mt-4">
+                <a href="https://github.com/walterporta/portfoliowp" target="_blank" rel="noopener noreferrer" 
+                className="mr-4 text-white bg-neutral-800 hover:text-cyan-500 border rounded-lg px-2 py-1">Repositorio</a>
+                {/* <a href="https://your-deploy-link" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 border border-blue-500 hover:border-blue-700 rounded px-2 py-1">Deploy</a> */}
+              </div>
             </div>
           )}
         </div>
       </div>
     </div>
-    )
+  )
 }
 
 export default PortfolioCard;
-
